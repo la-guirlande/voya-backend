e8aaa9acc15317adf4d298911491ea013f3018ae
Lelberto <jsurieux@gmail.com>
2021-03-28 14:24:15 +0200

    Add user information endpoint

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { UserInstance } from '../models/user-model';
 import ServiceContainer from '../services/service-container';
 import Controller, { Link } from './controller';
 
@@ -16,6 +17,7 @@ export default class UserController extends Controller {
    */
   public constructor(container: ServiceContainer) {
     super(container, '/users');
+    this.registerEndpoint({ method: 'GET', uri: '/info', handlers: [this.container.auth.authenticateHandler, this.container.auth.isAuthenticatedHandler, this.infoHandler] });
     this.registerEndpoint({ method: 'GET', uri: '/', handlers: this.listHandler });
     this.registerEndpoint({ method: 'GET', uri: '/:id', handlers: this.getHandler });
     this.registerEndpoint({ method: 'POST', uri: '/', handlers: this.createHandler });
@@ -24,6 +26,30 @@ export default class UserController extends Controller {
     this.registerEndpoint({ method: 'DELETE', uri: '/:id', handlers: this.deleteHandler });
     this.registerEndpoint({ method: 'GET', uri: '/:id/journeys', handlers: this.listJourneysHandler });
     this.registerEndpoint({ method: 'POST', uri: '/:id/journeys', handlers: this.createJourneyHandler });
+  }
+
+  /**
+     * Returns the authenticated user.
+     * 
+     * Path : `GET /users/info`
+     * 
+     * @param req Express request
+     * @param res Express response
+     * @async
+     */
+  public async infoHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      const authUser: UserInstance = res.locals.authUser;
+      if (authUser == null) {
+        return res.status(404).json(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'User not found'
+        }));
+      }
+      return res.status(200).json({ user: authUser });
+    } catch (err) {
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
   }
 
   /**
